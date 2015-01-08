ab3d64b8f08efab7dde078c9705464182c1215af
Rebecca Mills <vena.cava@gmail.com>
2015-01-07 21:16:58 -0500

    committing changes

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,11 +1,30 @@
 class NotificationsController < ApplicationController
 
-  def create
-    #add the code that will create a new notification and text it to the provided number 
+  def show
+  	@notification = Notification.find(params[:id])
+	end
+
+	def new
+		@notification = Notification.new
+	end
+
+	def create
+    @client.messages.create(
+  	from: '+14159341234',
+  	to: '+12403485428',
+  	body: 'Hey there!'
+	)	
   end
+
+
 
   private
   def notification_params
     #what are the params needed to make a new notification? (refer to your model)
   end
 end
+
+
+
+
+
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -5,17 +5,25 @@ class Notification
 
   def initialize(options={})
     self.to_number = options[:to_number]
-    account_sid = #ENV["twilio_sid"]
-    auth_token =  #ENV["twilio_token"]
+    account_sid = ENV["twilio_sid"]
+    auth_token =  ENV["twilio_token"]
     @client = Twilio::REST::Client.new(account_sid, auth_token)
   end
 
-  def text
-    # Sends a text to your phone number
-  end
 
   def random_compliment
-    compliments = [] #enter compliments there
+    [
+      "Thank God you look like your mother.", 
+      "Hi baby, how are you?",
+      "Call me if you need help on your pecs.",
+      "You've got a smile that lights up the room.",
+      "How old are you? Fifteen? I hope mom has a big fence.",
+      "Hi, how are you? I hope you have a big fence around the house.",
+      "You have one job. Keep the boys away from your sister.",
+      "Hey sister, you're beautiful.",
+      "As they say in Southern Delaware, you've done good, boy."
+    ].sample
+    #enter compliments there
     # Generates a random compliment to send to the textee
   end
 end
